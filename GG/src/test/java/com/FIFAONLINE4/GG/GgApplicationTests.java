421815dfbf93926f318a0d96f626c06f7b2c6b81
KimSeungJin <dahyun101107@naver.com>
2022-05-31 10:28:40 +0900

    Initial Project Setting

---
+++ GG/src/test/java/com/FIFAONLINE4/GG/GgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FIFAONLINE4.GG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
